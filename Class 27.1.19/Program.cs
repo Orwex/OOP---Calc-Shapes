35cb02002821759aa831df350b9149012abe5518
Orwex <44752155+Orwex@users.noreply.github.com>
2019-01-27 20:45:42 +0200

    Add files via upload

---
+++ Class 27.1.19/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2701
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle t = new Triangle
+            {
+                _a = 4,
+                _b = 5,
+                _c = 10,
+                _h = 8,
+            };
+
+            Circle c = new Circle
+            {
+                _r = 7.5
+            };
+
+            Console.WriteLine(t);
+            Console.WriteLine(c);
+
+            PrintCircleArea(c);
+            PrintTriangle(t);
+
+            TriangleRightAngle t3 = new TriangleRightAngle();
+            PrintTriangle(t3);
+
+        }
+
+        private static void PrintCircleArea(Circle c2)
+        {
+            c2.CalcArea();
+            Console.WriteLine(c2.areas);
+        }
+
+        private static void PrintTriangle(Triangle t2)
+        {
+            t2.CalcArea();
+            Console.WriteLine(t2.areas);
+        }
+
+        static void PrintAreasOfShape(ShapeBase s2)
+        {
+            s2.CalcArea();
+            Console.WriteLine(s2.areas);
+        {
+    }
+}
---
+++ Class 27.1.19/ShapeBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2701
+{
+    abstract class ShapeBase
+    {
+        public double areas;
+        public double hekef;
+
+        public abstract void CalcHekef();
+        public abstract void CalcArea();
+    }
+}
