e87baa4606373cb8201b4e326df6a60cb06a044b
Orwex <44752155+Orwex@users.noreply.github.com>
2019-01-27 19:47:01 +0200

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2701
+{
+    class Circle : ShapeBase
+    {
+        public double _r;
+
+        public override void CalcHekef()
+        {
+            hekef = Math.PI * 2 * _r;
+        }
+
+        public override void CalcArea()
+        {
+            areas = Math.PI * _r * _r;
+        }
+
+        public override string ToString()
+        {
+            return $"The area is : {areas}, The Perimeter is : {hekef}, The Radius is {_r}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2701
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle t = new Triangle
+            {
+                _a = 4,
+                _b = 5,
+                _c = 10,
+                _h = 8,
+            };
+
+            Circle c = new Circle
+            {
+                _r = 7.5
+            };
+
+            t.CalcArea();
+            t.CalcHekef();
+            Console.WriteLine(t.ToString());
+
+            c.CalcArea();
+            c.CalcHekef();
+            Console.WriteLine(c.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
